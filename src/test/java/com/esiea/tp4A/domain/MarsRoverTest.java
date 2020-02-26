5ad3eaedded07bd121f19cb60a978ab78716a136
Jessy ANDRIATSIMA <andriatsima@et.esiea.fr>
2020-02-26 14:31:42 +0100

    add spheric grid movements tests

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -5,14 +5,42 @@ import org.junit.jupiter.api.Test;
 
 public class MarsRoverTest {
 	
-	final MarsRover mr = new MarsRoverImpl().initialize(Position.of(0, 0, Direction.NORTH));
+	final MarsRover mr = new MarsRoverImpl().initialize(Position.of(0, 0, Direction.NORTH));	
+	final MarsRover mrRightEast = new MarsRoverImpl().initialize(Position.of(50, 0, Direction.EAST));
+	final MarsRover mrRightWest = new MarsRoverImpl().initialize(Position.of(50, 0, Direction.WEST));
+	final MarsRover mrLeftWest = new MarsRoverImpl().initialize(Position.of(-49, 0, Direction.WEST));
+	final MarsRover mrLeftEast = new MarsRoverImpl().initialize(Position.of(-49, 0, Direction.EAST));
+	final MarsRover mrTopNorth = new MarsRoverImpl().initialize(Position.of(0, 50, Direction.NORTH));
+	final MarsRover mrTopSouth = new MarsRoverImpl().initialize(Position.of(0, 50, Direction.SOUTH));
+	final MarsRover mrBottomSouth = new MarsRoverImpl().initialize(Position.of(0, -49, Direction.SOUTH));
+	final MarsRover mrBottomNorth = new MarsRoverImpl().initialize(Position.of(0, -49, Direction.NORTH));
 
 	@Test
 	void testMoveForward() {
 		Assertions.assertThat(mr.move("f"))
-				.as("Rover is moving forward (North) from origin")
-				.extracting(Position::getX, Position::getY, Position::getDirection)
-				.containsExactly(0, 1, Direction.NORTH);
+			.as("Rover is moving forward (North) from origin")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(0, 1, Direction.NORTH);
+		
+		Assertions.assertThat(mrRightEast.move("f"))
+			.as("Rover is moving forward (EAST) from right end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(-49, 0, Direction.EAST);
+		
+		Assertions.assertThat(mrLeftWest.move("f"))
+			.as("Rover is moving forward (WEST) from left end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(50, 0, Direction.WEST);
+		
+		Assertions.assertThat(mrTopNorth.move("f"))
+			.as("Rover is moving forward (NORTH) from top end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(0, -49, Direction.NORTH);
+		
+		Assertions.assertThat(mrBottomSouth.move("f"))
+			.as("Rover is moving forward (SOUTH) from left end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(0, 50, Direction.SOUTH);
 	}
 	
 	@Test
@@ -21,6 +49,26 @@ public class MarsRoverTest {
 				.as("Rover is moving backward (South) from origin")
 				.extracting(Position::getX, Position::getY, Position::getDirection)
 				.containsExactly(0, -1, Direction.NORTH);
+		
+		Assertions.assertThat(mrRightWest.move("b"))
+			.as("Rover is moving backward (WEST) from right end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(-49, 0, Direction.WEST);
+	
+		Assertions.assertThat(mrLeftEast.move("b"))
+			.as("Rover is moving backward (EAST) from left end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(50, 0, Direction.EAST);
+	
+		Assertions.assertThat(mrTopSouth.move("b"))
+			.as("Rover is moving backward (SOUTH) from top end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(0, -49, Direction.SOUTH);
+	
+		Assertions.assertThat(mrBottomNorth.move("b"))
+			.as("Rover is moving backward (NORTH) from left end")
+			.extracting(Position::getX, Position::getY, Position::getDirection)
+			.containsExactly(0, 50, Direction.NORTH);
 	}
 	
 	@Test
