b4ecd46c99f887c4624034c9d3665f95537b008c
Ephemeral25 <clemdem61@gmail.com>
2020-03-12 19:11:32 +0100

    Fixed @SpringBootConfiguration build error

--- src/test/java/com/esiea/tp4A/domain/DemoApplicationTests.java
+++ src/test/java/com/esiea/tp4A/domain/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.esiea.tp4A.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import Controller.ApiController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiController.class)
 class DemoApplicationTests {
 
 	@Test
