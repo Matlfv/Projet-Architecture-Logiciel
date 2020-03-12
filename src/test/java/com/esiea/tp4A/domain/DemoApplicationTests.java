344ee5f79c2a665f24e5fdbabfcd83a6e1a2ddeb
Ephemeral25 <clemdem61@gmail.com>
2020-03-12 19:16:04 +0100

    Fixed class RunWith error on travis cibuild

--- src/test/java/com/esiea/tp4A/domain/DemoApplicationTests.java
+++ src/test/java/com/esiea/tp4A/domain/DemoApplicationTests.java
@@ -1,13 +1,10 @@
 package com.esiea.tp4A.domain;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import Controller.ApiController;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApiController.class)
 class DemoApplicationTests {
 
